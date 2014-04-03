85f10be994f0c7cc21b826f12afbbfff0b0f95b8
Liam Reading <lsd.reading@gmail.com>
2014-04-03 15:01:22 +0200

    Added Activities
    TaskEdit and TaskArchive activity added

--- app/src/main/java/com/example/taskone/app/Notification.java
+++ app/src/main/java/com/example/taskone/app/Notification.java
@@ -1,7 +1,11 @@
 package com.example.taskone.app;
 
+import java.util.Date;
+
 /**
  * Created by lire on 03.04.2014.
  */
 public class Notification {
+   protected Date datum;
+    protected Date zeit;
 }
--- app/src/main/java/com/example/taskone/app/Task.java
+++ app/src/main/java/com/example/taskone/app/Task.java
@@ -1,7 +1,13 @@
 package com.example.taskone.app;
 
+import java.util.Date;
+
 /**
  * Created by lire on 03.04.2014.
  */
 public class Task {
+    protected Date datum;
+    protected Date zeit;
+    protected String notizen;
+    protected String titel;
 }
